d69f2a67e5944e0d702e5395f49f5acd9ffe3ed3
bhargavgurugubelli <gurugubellibhargav@gmail.com>
2025-08-02 22:14:54 +0530

    Add debug-media endpoint to list media/assets files

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import contact_submit
 
+from contact.views import list_media 
+
 urlpatterns = [
     path('', contact_submit, name='contact-submit'),
+    path("debug-media/", list_media)
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import Contact
+import os
+
 
 @csrf_exempt
 def contact_submit(request):
@@ -15,3 +17,10 @@ def contact_submit(request):
         return JsonResponse({'success': True, 'message': 'Message sent!'})
 
     return JsonResponse({'error': 'Only POST allowed'}, status=400)
+
+
+
+def list_media(request):
+    path = os.path.join(settings.MEDIA_ROOT, "assets")
+    files = os.listdir(path) if os.path.exists(path) else []
+    return JsonResponse({"files": files})
